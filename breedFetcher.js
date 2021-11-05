c48ca230afa1e3ef7b729c3d6ebaeb2f6c56c177
Morsal Niyaz <morsalniyaz@gmail.com>
2021-11-05 15:18:00 -0700

    Refactored Code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,26 @@
-const userInput = process.argv.slice(2);
-const search = userInput[0].toLowerCase();
-
 const request = require('request');
-const URL = `https://api.thecatapi.com/v1/breeds/search?q=${search}`;
 
-request(URL, (error, response, body) => {
-  // console.log(body);
-  // console.log(typeof body);
+const fetchBreedDescription = function(breedName, callback) {
 
-  if (error) {
-    console.log('error: ', error);
-    throw new Error('Something went wrong!', error);
-  }
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  console.log('breedName: ', breedName);
 
-  const data = JSON.parse(body);
-  const breed = data[0];
-  // console.log('data: ', data);
-  // console.log(typeof data);
+  request(URL, (error, response, body) => {
 
-  if (!breed) {
-    console.log(`Breed Not Found`);
-    return `Breed Not Found`;
-  }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    // console.log('data: ', data);
+    // console.log(typeof data);
+  
+    if (!breed) {
+      callback(`Failed to find ${breedName}`,null);
+    }
+  
+    if (breed) {
+      callback(null, breed.description);
+    }
+  });
 
-  if (breed) {
-    console.log(breed.description);
-  }
-});
+};
 
+module.exports = { fetchBreedDescription };
